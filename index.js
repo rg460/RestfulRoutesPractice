99cc3ed3045c0ea381b9b88b0e33e3f8ce661f15
ravigordon <rgordz460@outlook.com>
2022-05-06 10:29:07 +0100

    Restful API blueprint finished

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const app = express();
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 
 app.get('/tacos', (req, res) => {
     res.send("GET/tacos response");
 })
 app.post('/tacos', (req, res) => {
-    console.log(req.body);
-    res.send("POST/tacos response");
+    const {meat, qty} = req.body;
+    res.send(`Ok, here are your ${qty} ${meat} tacos`);
 })
 app.listen(3000,()=>{
     console.log('listening on port 3000');
 })
+
+// RESTFUL COMMENTS
+// GET / comments - list all comments
+// POST / comments - list all comments
+// GET / comments/:id - Get one comment (using ID)
+// PATCH / comments/:id - Patch one comment 
+// DELETE / comments/:id - Delete one comment
+
