fcd0c607bf7b7496a49673aea7d56ad88d313d18
ravigordon <rgordz460@outlook.com>
2022-05-06 10:37:13 +0100

    views path set up

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
-app.set('view engine','ejs')
+app.set('views',path.join(__dirname, 'views'))
+app.set('view engine','ejs');
 
 
 app.get('/tacos', (req, res) => {
